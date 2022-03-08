aff2d6d034003c90a3b85e659b22caaa5c98fabb
ivan <ivan.bakotin1999@gmail.com>
2022-03-08 17:32:50 +0100

    added image slider

---
+++ js/index.js
@@ -0,0 +1,17 @@
+const slides = document.querySelectorAll(".slide");
+const prev = document.querySelectorAll(".prev");
+const next = document.querySelectorAll(".next");
+
+let active = 0;
+
+for (let i = 0; i < 3; i++) {
+  prev[i].addEventListener("click", () => {
+    slides[i].classList.toggle("active")
+    slides[i ? i - 1 : 2].classList.toggle("active")
+  })
+
+  next[i].addEventListener("click", () => {
+    slides[i].classList.toggle("active")
+    slides[i ? 0 : i + 1].classList.toggle("active")
+  })
+}
